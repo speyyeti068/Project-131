716768fd53fc5236a7a69b4d863ea321102db709
speyyeti068 <75049648+speyyeti068@users.noreply.github.com>
2021-06-23 18:24:05 -0500

    Add files via upload

---
+++ kitchen.js
@@ -0,0 +1,57 @@
+img = "";
+status = "";
+objects = [];
+
+function preload(){
+    img = loadImage("kitchen.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(380,380);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+    video.size(380,380);
+
+    objectDetector = ml5.objectDetector("cocossd",modelLoaded);
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img,gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }else{
+        console.log(results);
+        objects = results;
+    }
+}
+
+function draw(){
+    image(img,0,0,380,380);
+
+    if(status != ""){
+        r = random(255);
+        g = random(255);
+        b = random(255);
+
+        objectDetector.detect(img,gotResult);
+
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Object(s) Being Detected";
+            document.getElementById("numberOfObjects").innerHTML = "Number Of Objects Detected Are " + objects.length
+
+            fill("#ff0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 5, objects[i].y + 15);
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);  
+        }
+    }
+}
